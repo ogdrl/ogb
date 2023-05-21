89db8e28d38e20654f2c362d5a1e58b65ec01ea9
ogdrl <63884311+ogdrl@users.noreply.github.com>
2023-05-21 18:10:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+function toggleTheme() {
+  const body = document.body;
+  const footer = document.querySelector('footer');
+  const themeToggle = document.querySelector('.theme-toggle');
+
+  body.classList.toggle('dark-mode');
+  footer.classList.toggle('dark-mode');
+  themeToggle.classList.toggle('dark-mode');
+
+  // Check if the browser supports haptic feedback
+  if ('vibrate' in navigator) {
+    navigator.vibrate(50); // Provide haptic feedback for 50 milliseconds
+  }
+}
+
+// Add animation to the dark mode button
+const themeToggle = document.querySelector('.theme-toggle');
+themeToggle.addEventListener('click', () => {
+  themeToggle.classList.add('animate');
+  setTimeout(() => {
+    themeToggle.classList.remove('animate');
+  }, 1000);
+});
+
 function nudgeScreen() {
   const mainElement = document.querySelector('main');
   mainElement.classList.add('nudge-screen');

9765198fc6b1ab748c811e119dfa85e7c2fe97a6
ogdrl <63884311+ogdrl@users.noreply.github.com>
2023-05-21 18:51:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Function to nudge the screen
 function nudgeScreen() {
   const mainElement = document.querySelector('main');
   mainElement.classList.add('nudge-screen');
@@ -7,23 +6,35 @@ function nudgeScreen() {
   }, 300);
 }
 
-// Function to redirect to a URL
 function redirectTo(url) {
   window.location.href = url;
 }
 
-// Function to toggle between light and dark mode
 function toggleTheme() {
   const bodyElement = document.body;
-  const footerElement = document.querySelector('footer');
   const themeToggle = document.querySelector('.theme-toggle');
-  
-  bodyElement.classList.toggle('light-mode');
-  footerElement.classList.toggle('light-mode');
-  themeToggle.classList.toggle('animate');
-  
-  // Haptic feedback
-  if (navigator.vibrate) {
-    navigator.vibrate(20);
+  const isDarkMode = bodyElement.classList.toggle('dark-mode');
+
+  if (isDarkMode) {
+    themeToggle.classList.add('animate');
+    themeToggle.setAttribute('aria-label', 'Switch to Light Mode');
+  } else {
+    themeToggle.classList.remove('animate');
+    themeToggle.setAttribute('aria-label', 'Switch to Dark Mode');
   }
+
+  // Store the user's preferred theme in localStorage
+  localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+}
+
+// Check the user's preferred theme from localStorage
+const userTheme = localStorage.getItem('theme');
+if (userTheme === 'dark') {
+  document.body.classList.add('dark-mode');
+}
+
+// Detect if the user's device prefers dark mode
+const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+if (prefersDarkMode && !userTheme) {
+  document.body.classList.add('dark-mode');
 }
